567be5e6b218246aacf6c10f889b6d9c49afd85c
GarrisonGoetsch <goetschgarrison@gmail.com>
2021-03-21 21:12:05 -0700

    Stable version - multiprocessing doesn't work, but the code for it is commented out.  Had to revert to old method of declaring global variables.

--- PDF_Getter/PDF_Getter/PDF_Getter.py
+++ PDF_Getter/PDF_Getter/PDF_Getter.py
@@ -24,25 +24,24 @@ path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 #This code tells the program where wkhtmltopdf.exe is on the machine.
 
-url_list = ['https://www.hollywoodreporter.com/features/racist-sexist-classic-how-hollywood-is-dealing-with-its-problematic-content', 'https://www.fakedoesntwork.com/news', 'https://www.greenwichtime.com/news/article/Cuomo-seeks-to-buy-time-amid-scandals-turns-to-15999489.php', 'https://www.wikipedia.org/', 'https://www.google.com/', 'https://en.wikipedia.org/wiki/Central_African_Republic', 'https://www.albanyherald.com/features/health/covid-19-death-rates-10-times-higher-in-countries-where-most-adults-are-overweight-report/article_5ee60b49-22a5-5f0d-935c-3893f786f4ec.html', 'https://www.boxofficemojo.com/date/2021-03-03/weekly/', 'https://www.the-sun.com/uncategorized/2442184/supersonic-jet-spike-london-new-york-inside/', 'https://www.the-sun.com/uncategorized/2442184/supersonic-jet-spike-london-new-york-inside/']
+url_list = ['https://www.wikipedia.org/', 'https://www.hollywoodreporter.com/features/racist-sexist-classic-how-hollywood-is-dealing-with-its-problematic-content', 'https://www.fakedoesntwork.com/news', 'https://www.greenwichtime.com/news/article/Cuomo-seeks-to-buy-time-amid-scandals-turns-to-15999489.php', 'https://www.wikipedia.org/', 'https://www.google.com/', 'https://en.wikipedia.org/wiki/Central_African_Republic', 'https://www.albanyherald.com/features/health/covid-19-death-rates-10-times-higher-in-countries-where-most-adults-are-overweight-report/article_5ee60b49-22a5-5f0d-935c-3893f786f4ec.html', 'https://www.boxofficemojo.com/date/2021-03-03/weekly/', 'https://www.the-sun.com/uncategorized/2442184/supersonic-jet-spike-london-new-york-inside/', 'https://www.the-sun.com/uncategorized/2442184/supersonic-jet-spike-london-new-york-inside/']
 #'https://www.wikipedia.org/', 'https://www.google.com/', 'https://en.wikipedia.org/wiki/Central_African_Republic']
 #This is a list of URL's that you want to be turne dinto PDFs.
 
 dest_Folder = r"C:/Users/Garrison/Documents/13th_Grade/geoparsing/SUMMER/PDFS"
     #This is the folder path you want PDFs to be written to.
 
+#GLOBAL VARIABLES
 global newpath
-global done_docs
-global errors
-errors = 0
-done_docs = 0
+newpath = "placeholder"
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% HERE AND ABOVE IS CONFIGURATION %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 def Find_Time():
+    global newpath
     lcTime = time.strftime("%Y%m%d%H%M", time.localtime())
     clock = lcTime[0 : 4] + "_" + lcTime[4 : 6] + "_" + lcTime[6 : 8] + "_" + lcTime[8 : 10] + "." + lcTime[10 : 12]
-    global newpath
-    newpath =  dest_Folder + "/" + str(clock)
+    newpath = dest_Folder + "/" + str(clock)
+    #dig into the line right above this one
     Make_Folder()
 def Make_Folder():
     Check_Folder_Name()
@@ -56,53 +55,41 @@ def Check_Folder_Name():
     #The above functions create a folder with a unique name based on the time.
 
 def Get_PDF(url):
-    global done_docs
-    global errors
-    name = "placeholder"
-    n1 = str(url)
-    n2 = n1.split('.')
-    n3 = []
-    for n in n2:
+    global newpath
+    name = url
+    n_list = name.split(".")
+    true_name = "placeholder"
+    for n in n_list:
         if "www" in n:
             pass
         elif "http" in n:
             pass
-        elif "com" in n:
-            pass
         else:
-            n3.append(n)
-    n4 = str(n3)    
-    if os.path.exists(newpath + "/" + n4 + "_pdf.pdf"):
-        n4
-    try:
-        pdfkit.from_url(url, newpath + "/" + n4 + "_pdf.pdf", options=opts, configuration=config)
-        done_docs += 1
-    except Exception as e:
+            true_name = str(n)
+            break
+    #why does globals()["newpath"] still = placeholder?
+    if os.path.exists(newpath + "/" + true_name + "_pdf.pdf"):
+        print("This already exists!")
+    else:
+        try:
+            pdfkit.from_url(url, newpath + "/" + true_name + "_pdf.pdf", options=opts, configuration=config)
+        except Exception as e:
            print(e)
    
 def Cleanup():
     print("Done printing all docs!")
 
 
-errors = 0
-done = 0
+
 if __name__ == "__main__":
     Find_Time()
     f_list = []
     for url in url_list:
-        p = multiprocessing.Process(target=Get_PDF, args=url)
-        p.start()
-        f_list.append(p)
-    for f in f_list:
-        f.join()
-Cleanup()
-
-        
-    #This function calls on other functions to create a new, unique folder.
-    #Then, this function generates PDFs from all urls in the list url_list and writes them to the new folder.
-    #It also prints helpful updates along the way.
-
-#to do:
-    #make an exception so that webpages that do not work are skipped
-        #also create a list of urls that were not able to print
-        #print the list to a txt file?
+        Get_PDF(url)
+#    for url in url_list:
+#        p = multiprocessing.Process(target=Get_PDF, args=[url])
+#        p.start()
+#        f_list.append(p)
+#    for f in f_list:
+#        f.join()
+#    Cleanup()
